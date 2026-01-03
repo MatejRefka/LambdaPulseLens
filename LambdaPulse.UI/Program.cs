405b297c8b35c67f417bd56c6503439c3f2ae1a3
Matej Refka <matejrefka@googlemail.com>
2026-01-03 15:41:15 +0000

    UI endpoint setup

--- LambdaPulse.UI/Program.cs
+++ LambdaPulse.UI/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LambdaPulse.Server;
+using LambdaPulse.Server.Services.Http.Routing;
+using LambdaPulse.Server.Utility.Extensions;
+
+var endpointRegistry = new EndpointRegistry();
+endpointRegistry.AddEndpoint(new Endpoint
+{
+    Method = "GET",
+    Path = "/api/health",
+    ApplicationFunction = async (webContext, cancellationToken) =>
+    {
+        webContext.WebResponse.StatusCode = 200;
+        webContext.WebResponse.ResponsePhrase = "OK";
+        await webContext.WebResponse.WriteToBody("Healthy");
+    }
+});
+
+var webServer = ServerBuilder.Build(endpointRegistry);
+await webServer.StartServer();
