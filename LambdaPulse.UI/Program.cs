7e42f3797e906885bfc3e93dae7f9ad32f5ca8ca
Matej Refka <matejrefka@googlemail.com>
2026-01-09 20:47:16 +0000

    Sealed low-level implementations and added interfaces. Refactored server build to allow for overriding registered implementations

--- LambdaPulse.UI/Program.cs
+++ LambdaPulse.UI/Program.cs
@@ -2,73 +2,75 @@
 using LambdaPulse.Server.Hosting;
 using LambdaPulse.Server.Shared.Extensions;
 
-var endpointRegistry = new EndpointRegistry();
-endpointRegistry.AddEndpoint(new Endpoint
-{
-    Method = "GET",
-    Path = "/",
-    ApplicationFunction = async (webContext, cancellationToken) =>
+var webServer = ServerBuilder.Build(
+    configureEndpoints: endpointRegistry =>
     {
-        webContext.StaticFileRelativePath = "/index.html";
-    }
-});
-endpointRegistry.AddEndpoint(new Endpoint
-{
-    Method = "GET",
-    Path = "/api/health",
-    ApplicationFunction = async (webContext, cancellationToken) =>
-    {
-        webContext.WebResponse.StatusCode = 200;
-        webContext.WebResponse.ResponsePhrase = "OK";
-
-        if (string.Equals(webContext.NegotiatedMimeType, "text/plain", StringComparison.OrdinalIgnoreCase))
+        endpointRegistry.AddEndpoint(new Endpoint
         {
-            await webContext.WebResponse.WriteStringToBody("Healthy", cancellationToken);
-            return;
-        }
-
-        await webContext.WebResponse.WriteJsonToBody(new { Health = "Healthy" }, cancellationToken);
-    }
-});
-endpointRegistry.AddEndpoint(new Endpoint
-{
-    Method = "GET",
-    Path = "/api/users/{userId}/orders/{orderId}",
-    ApplicationFunction = async (webContext, ct) =>
-    {
-        // Accessing the path parameter
-        var userId = webContext.Endpoint!.PathParameters["userId"];
-        var orderId = webContext.Endpoint!.PathParameters["orderId"];
-
-        var response = new
+            Method = "GET",
+            Path = "/",
+            ApplicationFunction = async (webContext, cancellationToken) =>
+            {
+                webContext.StaticFileRelativePath = "/index.html";
+            }
+        });
+        endpointRegistry.AddEndpoint(new Endpoint
         {
-            UserId = userId,
-            OrderId = orderId
-        };
+            Method = "GET",
+            Path = "/api/health",
+            ApplicationFunction = async (webContext, cancellationToken) =>
+            {
+                webContext.WebResponse.StatusCode = 200;
+                webContext.WebResponse.ResponsePhrase = "OK";
 
-        await webContext.WebResponse.WriteJsonToBody(response, ct);
-    }
-});
-endpointRegistry.AddEndpoint(new Endpoint
-{
-    Method = "GET",
-    Path = "/api/users/me/orders/{orderId}",
-    ApplicationFunction = async (webContext, ct) =>
-    {
-        var userId = "current user";
-        var orderId = webContext.Endpoint!.PathParameters["orderId"];
-        var pageNumber = webContext.WebRequest.QueryParameters["page"];
+                if (string.Equals(webContext.NegotiatedMimeType, "text/plain", StringComparison.OrdinalIgnoreCase))
+                {
+                    await webContext.WebResponse.WriteStringToBody("Healthy", cancellationToken);
+                    return;
+                }
 
-        var response = new
+                await webContext.WebResponse.WriteJsonToBody(new { Health = "Healthy" }, cancellationToken);
+            }
+        });
+        endpointRegistry.AddEndpoint(new Endpoint
         {
-            UserId = userId,
-            OrderId = orderId,
-            PageNumber = pageNumber
-        };
+            Method = "GET",
+            Path = "/api/users/{userId}/orders/{orderId}",
+            ApplicationFunction = async (webContext, ct) =>
+            {
+                // Accessing the path parameter
+                var userId = webContext.Endpoint!.PathParameters["userId"];
+                var orderId = webContext.Endpoint!.PathParameters["orderId"];
 
-        await webContext.WebResponse.WriteJsonToBody(response, ct);
-    }
-});
+                var response = new
+                {
+                    UserId = userId,
+                    OrderId = orderId
+                };
 
-var webServer = ServerBuilder.Build(endpointRegistry);
+                await webContext.WebResponse.WriteJsonToBody(response, ct);
+            }
+        });
+        endpointRegistry.AddEndpoint(new Endpoint
+        {
+            Method = "GET",
+            Path = "/api/users/me/orders/{orderId}",
+            ApplicationFunction = async (webContext, ct) =>
+            {
+                var userId = "current user";
+                var orderId = webContext.Endpoint!.PathParameters["orderId"];
+                var pageNumber = webContext.WebRequest.QueryParameters["page"];
+
+                var response = new
+                {
+                    UserId = userId,
+                    OrderId = orderId,
+                    PageNumber = pageNumber
+                };
+
+                await webContext.WebResponse.WriteJsonToBody(response, ct);
+            }
+        });
+    });
+
 await webServer.StartServer();
