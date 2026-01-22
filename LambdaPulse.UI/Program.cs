885575f7052fbcc946fd8088b25eb98128305f0b
Matej Refka <matejrefka@googlemail.com>
2026-01-22 22:25:31 +0000

    Setup LambdaPulse.UI as the UI host for build artifacts

--- LambdaPulse.UI/Program.cs
+++ LambdaPulse.UI/Program.cs
@@ -2,79 +2,69 @@
 using LambdaPulse.Server.Hosting;
 using LambdaPulse.Server.Shared.Extensions;
 
+
+
+#region Static pages
+
+var loginPage = new Endpoint
+{
+    Method = "GET",
+    Path = "/login",
+    ApplicationFunction = async (webContex, cancellationToken) =>
+    {
+        webContex.StaticFileRelativePath = "/login/index.html";
+    },
+    AllowAnonymous = true,
+};
+
+var registerPage = new Endpoint
+{
+    Method = "GET",
+    Path = "/register",
+    ApplicationFunction = async (webContex, cancellationToken) =>
+    {
+        webContex.StaticFileRelativePath = "/register/index.html";
+    },
+    AllowAnonymous = true,
+};
+
+var homePage = new Endpoint
+{
+    Method = "GET",
+    Path = "/",
+    ApplicationFunction = async (webContext, cancellationToken) =>
+    {
+        webContext.StaticFileRelativePath = "/home/index.html";
+    },
+    AllowAnonymous = true
+};
+
+#endregion Static pages
+
+#region API
+
+var healtCheck = new Endpoint
+{
+    Method = "GET",
+    Path = "/api/health",
+    ApplicationFunction = async (webContext, cancellationToken) =>
+    {
+        webContext.WebResponse.StatusCode = 200;
+        webContext.WebResponse.ResponsePhrase = "OK";
+        await webContext.WebResponse.WriteJsonToBody(new { Health = "Healthy" }, cancellationToken);
+    },
+    AllowAnonymous = true
+};
+
+#endregion API
+
 var webServer = ServerBuilder.Build(
     configureEndpoints: endpointRegistry =>
     {
-        endpointRegistry.AddEndpoint(new Endpoint
-        {
-            Method = "GET",
-            Path = "/",
-            ApplicationFunction = async (webContext, cancellationToken) =>
-            {
-                webContext.StaticFileRelativePath = "/index.html";
-            },
-            AllowAnonymous = true
-        });
-        endpointRegistry.AddEndpoint(new Endpoint
-        {
-            Method = "GET",
-            Path = "/api/health",
-            ApplicationFunction = async (webContext, cancellationToken) =>
-            {
-                webContext.WebResponse.StatusCode = 200;
-                webContext.WebResponse.ResponsePhrase = "OK";
-
-                if (string.Equals(webContext.NegotiatedMimeType, "text/plain", StringComparison.OrdinalIgnoreCase))
-                {
-                    await webContext.WebResponse.WriteStringToBody("Healthy", cancellationToken);
-                    return;
-                }
-
-                await webContext.WebResponse.WriteJsonToBody(new { Health = "Healthy" }, cancellationToken);
-            },
-            AllowAnonymous = false
-        });
-        endpointRegistry.AddEndpoint(new Endpoint
-        {
-            Method = "GET",
-            Path = "/api/users/{userId}/orders/{orderId}",
-            ApplicationFunction = async (webContext, ct) =>
-            {
-                // Accessing the path parameter
-                var userId = webContext.Endpoint!.PathParameters["userId"];
-                var orderId = webContext.Endpoint!.PathParameters["orderId"];
-
-                var response = new
-                {
-                    UserId = userId,
-                    OrderId = orderId
-                };
-
-                await webContext.WebResponse.WriteJsonToBody(response, ct);
-            },
-            RequiredRole = "Orders"
-        });
-        endpointRegistry.AddEndpoint(new Endpoint
-        {
-            Method = "GET",
-            Path = "/api/users/me/orders/{orderId}",
-            ApplicationFunction = async (webContext, ct) =>
-            {
-                var userId = "current user";
-                var orderId = webContext.Endpoint!.PathParameters["orderId"];
-                var pageNumber = webContext.WebRequest.QueryParameters["page"];
-
-                var response = new
-                {
-                    UserId = userId,
-                    OrderId = orderId,
-                    PageNumber = pageNumber
-                };
-
-                await webContext.WebResponse.WriteJsonToBody(response, ct);
-            },
-            RequiredRole = "Orders"
-        });
+        endpointRegistry.AddEndpoint(loginPage);
+        endpointRegistry.AddEndpoint(registerPage);
+        endpointRegistry.AddEndpoint(homePage);
+        endpointRegistry.AddEndpoint(healtCheck);
     },
     configureServices: container =>
     {
