74b335c8e99b0cb61361fb8416b400b49e518319
Matej Refka <matejrefka@googlemail.com>
2026-03-26 21:57:48 +0000

    Inspector frame grid layout

--- LambdaPulse.Client/src/components/inspector/InspectorFrame.tsx
+++ LambdaPulse.Client/src/components/inspector/InspectorFrame.tsx
@@ -8,6 +8,18 @@ interface InspectorFrameProps {
   trace: Trace;
 }
 
+//request/response pill
+const WebContextPill = ({ type }: { type: "Request" | "Response" }) => (
+  <div className="px-4 py-1.5 rounded-full bg-surface-20 border border-surface-40 text-[10px] uppercase font-bold text-surface-60 tracking-wider shadow-sm z-10">
+    HTTP {type}
+  </div>
+);
+
+//container for VerticalWire -> WebContextPill -> VerticalWire
+const FlexWireContainer = ({ children }: { children: React.ReactNode }) => (
+  <div className="flex flex-col items-center flex-1 min-h-0 w-full">{children}</div>
+);
+
 export const InspectorFrame = ({ trace }: InspectorFrameProps) => {
   const getRequestStep = (middlewareName: string): Step | undefined => {
     return trace.pipeline.find(
@@ -26,11 +38,38 @@ export const InspectorFrame = ({ trace }: InspectorFrameProps) => {
   };
 
   return (
-    <div className="grid grid-cols-5 gap-6 w-full max-w-7xl mx-auto h-full">
-      {/*column 1, request mw 1 to 10*/}
-      <div className="flex flex-col h-full">
-        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_1_10[0])} flowDirection="request" isFlex />
+    //4 rows: flex wire -> pipeline -> termination node -> flex wire
+    <div className="grid grid-cols-[1fr_1fr_4rem_1fr_1fr] grid-rows-[1fr_auto_auto_1fr] gap-x-6 w-full max-w-7xl mx-auto h-full">
+      {/*row 1 col 1, flex wire with Http Request pill*/}
+      <div className="row-start-1 col-start-1 flex flex-col h-full">
+        <FlexWireContainer>
+          <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_1_10[0])} flowDirection="request" isFlex />
+          <WebContextPill type="Request" />
+          <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_1_10[0])} flowDirection="request" isFlex />
+        </FlexWireContainer>
+      </div>
 
+      {/*row 1 col 2, flex wire*/}
+      <div className="row-start-1 col-start-2 flex flex-col h-full">
+        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_11_20[0])} flowDirection="request" isFlex />
+      </div>
+
+      {/*row 1 col 4, flex wire*/}
+      <div className="row-start-1 col-start-4 flex flex-col h-full">
+        <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_11_20[0])} flowDirection="response" isFlex />
+      </div>
+
+      {/*row 1 col 5, flex wire with Http Response pill*/}
+      <div className="row-start-1 col-start-5 flex flex-col h-full">
+        <FlexWireContainer>
+          <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_1_10[0])} flowDirection="response" isFlex />
+          <WebContextPill type="Response" />
+          <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_1_10[0])} flowDirection="response" isFlex />
+        </FlexWireContainer>
+      </div>
+
+      {/*row 2 col 1, request mw 1 to 10*/}
+      <div className="row-start-2 col-start-1 flex flex-col">
         {MIDDLEWARE_1_10.map((middlewareName, index) => {
           const step = getRequestStep(middlewareName);
           const isLast = index === MIDDLEWARE_1_10.length - 1;
@@ -42,11 +81,11 @@ export const InspectorFrame = ({ trace }: InspectorFrameProps) => {
           );
         })}
 
-        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_1_10[9])} flowDirection="request" isFlex />
+        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_1_10[9])} flowDirection="request" />
       </div>
 
-      {/*column 2, request mw 11 to 20*/}
-      <div className="flex flex-col h-full">
+      {/*row 2 col 2, request mw 11 to 20*/}
+      <div className="row-start-2 col-start-2 flex flex-col">
         <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_11_20[0])} flowDirection="request" isFlex />
 
         {MIDDLEWARE_11_20.map((middlewareName, index) => {
@@ -60,18 +99,11 @@ export const InspectorFrame = ({ trace }: InspectorFrameProps) => {
           );
         })}
 
-        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_11_20[9])} flowDirection="request" isFlex />
+        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_11_20[9])} flowDirection="request" />
       </div>
 
-      {/*column 3, termination mw*/}
-      <div className="flex flex-col items-center justify-end pb-12 h-full">
-        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_TERMINATION)} flowDirection="request" />
-        <MiddlewareNode middlewareName={MIDDLEWARE_TERMINATION} step={getRequestStep(MIDDLEWARE_TERMINATION)} />
-        <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_TERMINATION)} flowDirection="response" />
-      </div>
-
-      {/*column 4, response mw 1-10*/}
-      <div className="flex flex-col h-full">
+      {/*row 2 col 4, response mw 1-10*/}
+      <div className="row-start-2 col-start-4 flex flex-col">
         <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_11_20[0])} flowDirection="response" isFlex />
 
         {MIDDLEWARE_11_20.map((middlewareName, index) => {
@@ -87,12 +119,11 @@ export const InspectorFrame = ({ trace }: InspectorFrameProps) => {
             </React.Fragment>
           );
         })}
-        <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_11_20[9])} flowDirection="response" isFlex />
+        <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_11_20[9])} flowDirection="response" />
       </div>
 
-      {/*column 5, response mw 11-20*/}
-      <div className="flex flex-col h-full">
-        <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_1_10[0])} flowDirection="response" isFlex />
+      {/*row 2 col 5, response mw 11-20*/}
+      <div className="row-start-2 col-start-5  flex flex-col">
         {MIDDLEWARE_1_10.map((middlewareName, index) => {
           const step = getResponseStep(middlewareName);
           const isLast = index === MIDDLEWARE_1_10.length - 1;
@@ -107,6 +138,31 @@ export const InspectorFrame = ({ trace }: InspectorFrameProps) => {
           );
         })}
 
+        <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_1_10[9])} flowDirection="response" />
+      </div>
+
+      {/*row 3 col 1, filler wire*/}
+      <div className="row-start-3 col-start-1 h-full flex flex-col">
+        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_1_10[9])} flowDirection="request" isFlex />
+      </div>
+
+      {/*row 3 col 2 + 3 + 4, termination node*/}
+      <div className="row-start-3 col-start-2 col-span-3 w-full flex items-center justify-center relative">
+        <MiddlewareNode middlewareName={MIDDLEWARE_TERMINATION} step={getRequestStep(MIDDLEWARE_TERMINATION)} />
+      </div>
+
+      {/*row 3 col 5, filler wire*/}
+      <div className="row-start-3 col-start-5  h-full flex flex-col">
+        <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_1_10[9])} flowDirection="response" isFlex />
+      </div>
+
+      {/*row 4 col 1, flex wire*/}
+      <div className="row-start-4 col-start-1  h-full flex flex-col">
+        <VerticalWire isActive={!!getRequestStep(MIDDLEWARE_1_10[9])} flowDirection="request" isFlex />
+      </div>
+
+      {/*row 4 col 5, flex wire*/}
+      <div className="row-start-4 col-start-5 h-full flex flex-col">
         <VerticalWire isActive={!!getResponseStep(MIDDLEWARE_1_10[9])} flowDirection="response" isFlex />
       </div>
     </div>
