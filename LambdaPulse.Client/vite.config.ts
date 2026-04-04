06eb2059e8808222233b81eaa1f46bf95500b1d7
Matej Refka <matejrefka@googlemail.com>
2026-04-04 14:00:25 +0100

    Added Vite Checker Plugin + update vulnerable packages

--- LambdaPulse.Client/vite.config.ts
+++ LambdaPulse.Client/vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        tsconfigPath: "./tsconfig.app.json"
+      }
+    }),
+    tailwindcss()
+  ],
   build: {
     outDir: path.resolve(__dirname, "../LambdaPulse.UI/wwwroot"),
     emptyOutDir: true
   }
-})
+});
