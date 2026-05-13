604a9eec581ac4f1c737e2da6870bee0e183c3ff
Matej Refka <matejrefka@googlemail.com>
2026-05-13 20:14:00 +0100

    SPA middleware + logging

--- src/LambdaPulseLens.Server/Program.cs
+++ src/LambdaPulseLens.Server/Program.cs
@@ -4,45 +4,6 @@ using LambdaPulse.Engine.Hosting;
 using LambdaPulse.Engine.Shared.Extensions;
 using LambdaPulse.Server.Services;
 
-#region Static pages
-
-var loginPage = new Endpoint
-{
-    Method = "GET",
-    Path = "/login",
-    ApplicationFunction = async (webContext, cancellationToken) =>
-    {
-        webContext.StaticFileRelativePath = "/login/index.html";
-    },
-    AllowAnonymous = true,
-};
-
-var registerPage = new Endpoint
-{
-    Method = "GET",
-    Path = "/register",
-    ApplicationFunction = async (webContext, cancellationToken) =>
-    {
-        webContext.StaticFileRelativePath = "/register/index.html";
-    },
-    AllowAnonymous = true,
-};
-
-var homePage = new Endpoint
-{
-    Method = "GET",
-    Path = "/",
-    ApplicationFunction = async (webContext, cancellationToken) =>
-    {
-        webContext.StaticFileRelativePath = "/home/index.html";
-    },
-    AllowAnonymous = true
-};
-
-#endregion Static pages
-
-#region API
-
 var healthCheck = new Endpoint
 {
     Method = "GET",
@@ -56,17 +17,12 @@ var healthCheck = new Endpoint
     AllowAnonymous = true
 };
 
-#endregion API
-
 //Postgres DB storing trace logs
 var postgresConnection = Environment.GetEnvironmentVariable("LAMBDAPULSE_POSTGRES_CONNECTION") ?? throw new InvalidOperationException("LAMBDAPULSE_POSTGRES_CONNECTION environment variable is not set.");
 
 var webServer = ServerBuilder.Build(
     configureEndpoints: endpointRegistry =>
     {
-        endpointRegistry.AddEndpoint(loginPage);
-        endpointRegistry.AddEndpoint(registerPage);
-        endpointRegistry.AddEndpoint(homePage);
         endpointRegistry.AddEndpoint(healthCheck);
     },
     configureServices: container =>
